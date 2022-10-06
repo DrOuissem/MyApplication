eda4a3933628ab8544b41c1d12264cf36bed2a0c
tony <o.benhenia@gmail.com>
2022-10-05 21:43:48 -0500

    new version

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,32 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var list=arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle("DinnerDecider");
     }
 
-    fun myFunction(view: View) {
-        var tv: TextView = findViewById<TextView>(R.id.tv1)
-        tv.text = "Toz"
+    fun decideFunction(view: View) {
+        val rand= Random.nextInt(0 , list.size)
+        var textview = findViewById<TextView>(R.id.selectedFood)
+        textview.text=list.get(rand)
+    }
+
+    fun addNewFood(view: View) {
+        val newFood = findViewById<EditText>(R.id.enterFood).text
+        if(!newFood.trim().equals("")){
+            list.add(newFood.toString())
+            findViewById<EditText>(R.id.enterFood).setText("")
+        }
+
     }
 }
